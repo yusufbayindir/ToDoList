1772c4c1701fcd05001d94db84f150c45f57f77f
yusufbayindir <bayindiryusuf0@gmail.com>
2024-03-19 05:55:39 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Yusuf Bayindir on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
